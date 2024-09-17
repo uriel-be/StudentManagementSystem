378b4ad9662d4fa6eb14b3b6c57189266e60f14d
uriel-be <ur.bendahan@gmail.com>
2024-09-17 07:36:13 +0300

    finish build logger func

--- logger.py
+++ logger.py
@@ -3,14 +3,11 @@ import enum
 import os
 
 
-class Level(enum):
-    DEBUG = logging.DEBUG,
-    INFO = logging.INFO,
-    WARN = logging.WARN,
-    ERROR = logging.ERROR
-
-
-def build_logger(path: str, level: Level = Level.DEBUG, file_name='log.log'):
+def build_logger(path: str, level: str='DEBUG', file_name='log.log'):
+    logger_levels = ['CRITICAL', 'FATAL', 'ERROR', 'WARNING', 'WARN', 'INFO', 'DEBUG', 'NOTSET']
+    if level.upper() not in logger_levels:
+        raise ValueError(f'level most be in {logger_levels}')
     if not os.path.exists(path):
         os.mkdir(path)
-    logging.basicConfig(filename=f'{path}/{file_name}', encoding='utf-8', level=level)
+    logging.basicConfig(filename=f'{path}/{file_name}', encoding='utf-8')
+    logging.getLogger('logger').setLevel(level)
